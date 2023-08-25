fd92ce4f15a9c3b58f4c3020c458e6de64a13897
Leon <leonchike@gmail.com>
2023-08-25 12:16:21 -0400

    removed og:image duplication

--- src/components/layouts/DefaultPage/DefaultPage.tsx
+++ src/components/layouts/DefaultPage/DefaultPage.tsx
@@ -7,14 +7,7 @@ const DefaultPage = ({ children }: { children: React.ReactNode }) => {
   return (
     <>
       <Head>
-        <title>Chike Nwankwo</title>
-        <link rel="icon" href="/favicon.png" />
-        <meta name="description" content="Chike Nwankwo's personal site" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <meta
-          property="og:image"
-          content="https://res.cloudinary.com/dch8jsjcf/image/upload/v1678486644/og-image_vkxd1b.png"
-        />
       </Head>
       <Wrapper>
         <div>{children}</div>
