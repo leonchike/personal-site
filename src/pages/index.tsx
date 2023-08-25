c374772e498ff671f45313030966bc11ab772ba1
Leon <leonchike@gmail.com>
2023-08-25 12:08:44 -0400

    added og data

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -14,7 +14,6 @@ const Home = () => {
     <>
       <Head>
         <title>Chike Nwankwo</title>
-        <meta name="description" content="Chike Nwankwo's personal site" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.png" />
       </Head>
