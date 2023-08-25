9b3303a242664875cc15f89501baa50ecd13e17f
Leon <leonchike@gmail.com>
2023-08-25 11:39:35 -0400

    added og data

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,6 +5,17 @@ export default function Document() {
     <Html lang="en">
       <Head>
         <meta name="theme-color" content="#fafafa" />
+        <meta property="og:title" content="Chike Nwankwo - Personal Site" />
+        <meta property="og:site_name" content="Chike Nwankwo - Personal Site" />
+        <meta
+          property="og:description"
+          content="A space to showcase my experiences and projects."
+        />
+        <meta
+          property="og:image"
+          content="https://imagedelivery.net/camphNQlX5poswEZJbu_Cw/5e091063-f025-4510-b7f9-5039b381dd00/public"
+        />
+        <meta property="og:type" content="website" />
       </Head>
       <body>
         <Main />
