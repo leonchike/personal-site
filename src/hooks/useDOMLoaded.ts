b8ce99fb22473db2e1dce08e491cccd648a64c1c
Leon <leonchike@gmail.com>
2023-08-25 11:21:17 -0400

    improve fade in and content

--- src/hooks/useDOMLoaded.ts
+++ src/hooks/useDOMLoaded.ts
@@ -4,17 +4,7 @@ const useDOMLoaded = (): boolean => {
   const [isLoaded, setIsLoaded] = useState<boolean>(false);
 
   useEffect(() => {
-    const handleContentLoaded = () => {
-      setIsLoaded(true);
-    };
-
-    if (document.readyState === "complete") {
-      setIsLoaded(true);
-    } else {
-      window.addEventListener("DOMContentLoaded", handleContentLoaded);
-      return () =>
-        window.removeEventListener("DOMContentLoaded", handleContentLoaded);
-    }
+    setIsLoaded(true);
   }, []);
 
   return isLoaded;
