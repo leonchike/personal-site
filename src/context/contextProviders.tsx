1f5dd5494f749418d1ff26738823a8d611b4d092
Leon <leonchike@gmail.com>
2023-10-16 18:42:49 -0400

    removed unused components

--- src/context/contextProviders.tsx
+++ src/context/contextProviders.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import GoogleAnalytics from "@/utils/analytics";
-import { AppProvider } from "@/context/appContext";
 import { MDXProvider } from "@mdx-js/react";
 import MDXComponentStyles from "@/styles/MDXComponentStyles";
 
@@ -9,9 +8,7 @@ const AppProviders = ({ children }: { children: React.ReactNode }) => {
   return (
     <>
       <GoogleAnalytics />
-      <AppProvider>
-        <MDXProvider components={MDXComponentStyles}>{children}</MDXProvider>
-      </AppProvider>
+      <MDXProvider components={MDXComponentStyles}>{children}</MDXProvider>
     </>
   );
 };
