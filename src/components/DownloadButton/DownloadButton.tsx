75f200967e9f21df342759dc1482890639700022
Leon <leonchike@gmail.com>
2024-03-28 00:44:42 -0400

    updated text and dependencies

--- src/components/DownloadButton/DownloadButton.tsx
+++ src/components/DownloadButton/DownloadButton.tsx
@@ -7,15 +7,16 @@ interface Props {
   onClick: () => void;
 }
 
-const DownloadButton = ({ children, ...props }: Props) => {
+export default function DownloadButton({ children, ...props }: Props) {
   return (
-    <button className={styles.button} {...props}>
-      {children}{" "}
-      <div className={styles.iconWrapper}>
+    <button
+      className="flex items-center border-2 border-gray-900 text-gray-900 rounded px-2 py-2 gap-2 hover:bg-gray-800 hover:text-white transition-all duration-500"
+      {...props}
+    >
+      <span>{children}</span>
+      <div className="">
         <Icon id="arrowDown" size="1.2rem" strokeWidth="2px" />
       </div>
     </button>
   );
-};
-
-export default DownloadButton;
+}
