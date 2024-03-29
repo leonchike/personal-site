97aae866ee85101b7c032a82303cc5a48f8d1364
Leon <leonchike@gmail.com>
2024-03-29 15:01:33 -0400

    added key

--- src/lib/get-ip-location.ts
+++ src/lib/get-ip-location.ts
@@ -1,12 +1,11 @@
 export async function getIpLocation(ipAddress: string) {
   if (!ipAddress) throw new Error("IP address is required");
 
-  const API_KEY = process.env.IP_GEOLOCATION_API_KEY;
-  if (!API_KEY) throw new Error("IP Geolocation API key is not set");
+  const KEY = "395e687e910c4dee81c82529f176df94";
 
   const API_URL_BASE = "https://api.ipgeolocation.io/ipgeo";
 
-  const url = `${API_URL_BASE}?apiKey=${API_KEY}&ip=${ipAddress}`;
+  const url = `${API_URL_BASE}?apiKey=${KEY}&ip=${ipAddress}`;
 
   const response = await fetch(url);
 
