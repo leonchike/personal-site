e13c1d6453c9edf20ef1b0e7887846c97e6ec1e2
Leon <leonchike@gmail.com>
2024-04-03 17:46:01 -0400

    remove env

--- src/components/ui/layout.tsx
+++ src/components/ui/layout.tsx
@@ -1,7 +1,7 @@
 export function WideWidthWrapper({ children }: { children: React.ReactNode }) {
   return (
     <div className="max-w-[160rem] m-auto">
-      <div className=" mx-2 md:mx-8">{children}</div>
+      <div className="mx-2 md:mx-8">{children}</div>
     </div>
   );
 }
