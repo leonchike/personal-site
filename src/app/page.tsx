5b5083c2d2839d5be776a4b13b5073211f29823a
Leon <leonchike@gmail.com>
2024-04-03 17:18:37 -0400

    installed tailwindcss

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 import Image from "next/image";
 
 export default function Home() {
-  return <div>Hello</div>;
+  return <div className="text-4xl text-red-600">Hello</div>;
 }
---
+++ src/app/typefaces/type-faces.tsx
@@ -0,0 +1,13 @@
+import localFont from "next/font/local";
+
+export const elzaVariable = localFont({
+  src: "./elzavariable-upright.woff2",
+  display: "swap",
+  variable: "--font-elzavariable",
+});
+
+export const gallient = localFont({
+  src: "./gallient-regular.woff",
+  display: "swap",
+  variable: "--font-gallient",
+});
