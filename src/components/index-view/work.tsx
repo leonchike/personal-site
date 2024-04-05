b99e2d8757c469b02e677bd0610be7803799bf3f
Leon <leonchike@gmail.com>
2024-04-05 16:24:41 -0400

    added work item 2

--- src/components/index-view/work.tsx
+++ src/components/index-view/work.tsx
@@ -15,7 +15,7 @@ export default async function WorkSection() {
       className="pt-8 md:pt-10"
     >
       <Heading />
-      <div className="flex flex-col gap-8">
+      <div className="flex flex-col gap-6">
         {workData.map((item, index) => (
           <WorkItem key={index} data={item} />
         ))}
@@ -36,7 +36,7 @@ function WorkItem({ data }: { data: WorkItemType }) {
   console.log(data);
 
   return (
-    <article className="space-y-8 md:space-y-12 pb-24 md:pb-28">
+    <article className="space-y-8 md:space-y-12 pb-16 md:pb-18">
       <WorkItemDescription data={data} />
       <WorkItemVisuals data={data} />
     </article>
@@ -79,6 +79,42 @@ function WorkItemVisuals({
   minItemWidth?: string;
   maxItemWidth?: string;
 }) {
+  if (!data.visuals) return null;
+
+  if (data.visuals.length === 0) return null;
+
+  if (data.displayType === "multi-images")
+    return <MultipleAssets data={data} />;
+
+  if (data.displayType === "single-image") return <SingleImage data={data} />;
+
+  // return (
+  //   <div className="flex justify-center">
+  //     <div className=" max-w-[1600px] w-full grid grid-cols-[repeat(auto-fill,minmax(min(480px,100%),1fr))] gap-x-4 gap-y-6 w-content">
+  //       {data.visuals.map((visual, index) => (
+  //         <div key={index} className="flex flex-col items-center">
+  //           <div className="flex flex-col items-center">
+  //             <img
+  //               src={visual.url}
+  //               alt={visual.alt}
+  //               className="w-[70%] md:w-[50%] lg:w-[70%] xl:w-[80%] h-auto"
+  //             />
+  //           </div>
+  //           <div className="py-8">
+  //             <p className="text-center px-4 max-w-[380px] text-[0.92rem]">
+  //               {visual.description}
+  //             </p>
+  //           </div>
+  //         </div>
+  //       ))}
+  //     </div>
+  //   </div>
+  // );
+
+  return null;
+}
+
+function MultipleAssets({ data }: { data: WorkItemType }) {
   return (
     <div className="flex justify-center">
       <div className=" max-w-[1600px] w-full grid grid-cols-[repeat(auto-fill,minmax(min(480px,100%),1fr))] gap-x-4 gap-y-6 w-content">
@@ -98,6 +134,36 @@ function WorkItemVisuals({
             </div>
           </div>
         ))}
+      </div>
+    </div>
+  );
+}
+
+function SingleImage({ data }: { data: WorkItemType }) {
+  const bgColor = data.visuals[0]?.bgColor;
+  const largeImage = data.visuals[0].url;
+  const smallImage = data.visuals[0].smallurl;
+
+  return (
+    <div className={`flex justify-center ${bgColor && `bg-[${bgColor}]`}`}>
+      <div className="flex flex-col items-center">
+        <img
+          src={smallImage}
+          alt={data.visuals[0].alt}
+          className="w-full lg:hidden"
+        />
+        <img
+          src={largeImage}
+          alt={data.visuals[0].alt}
+          className="hidden lg:block lg:w-full"
+        />
+        {data.visuals[0].description && (
+          <div className="py-8">
+            <p className="text-center px-4 max-w-[380px] text-[0.92rem]">
+              {data.visuals[0].description}
+            </p>
+          </div>
+        )}
       </div>
     </div>
   );
