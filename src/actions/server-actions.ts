0af0ec53013077e76ddc4c07b63305de1b4819ae
Leon <leonchike@gmail.com>
2024-04-08 13:25:19 -0400

    leadership

--- src/actions/server-actions.ts
+++ src/actions/server-actions.ts
@@ -2,11 +2,13 @@
 
 import aboutData from "@/data/about.data";
 import workData from "@/data/work.data";
+import leadershipData from "@/data/leadership.data";
 
 export async function getIndexData() {
   const data = {
     aboutData,
     workData,
+    leadershipData,
   };
 
   return data;
---
+++ src/data/leadership.data.ts
@@ -0,0 +1,29 @@
+/* eslint-disable import/no-anonymous-default-export */
+export default {
+  sectionStatement:
+    "Shipping products is great, but what I find even more meaningful is helping others grow. I offer pro-bono mentorship to newcomers in the field trying to get their first consulting or software engineering job, and those switching careers.",
+  calendlyLink: "https://calendly.com/leonchike",
+  leadershipData: [
+    {
+      heading: "300+",
+      subheading: "Mentorship sessions",
+      description:
+        "Hold weekly 1:1's with product managers, designers, and engineers to help educate, guide and identify areas for improvement.",
+    },
+    {
+      heading: "60+",
+      subheading: "Onboarding sessions",
+      description:
+        "Conduct onboarding sessions for new hires, and help them understand the company's mission, vision, and values.",
+    },
+  ],
+  highlights: [
+    "Facilitate workshops on design thinking, product management, and software engineering",
+    "Instructor (Part-time) at General Assembly, teaching Product Management",
+    "Mentor at the University of Maryland's Startup Shell, helping students in the early stages of their startup journey",
+    "Led product and design trainings at Deloitte Digital",
+    "Built design systems and led design sprints at Deloitte Digital",
+    "Provided 1:1 mentorship to team members at Deloitte Digital",
+    "Entrepreneurial Instructor at the University of Maryland's Dingman Center for Entrepreneurship",
+  ],
+};
