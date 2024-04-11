25474591e74b4d508015040839e0519c2e61dc6f
Leon <leonchike@gmail.com>
2024-04-10 20:01:20 -0400

    added admin

---
+++ src/components/admin-view/admin-view.tsx
@@ -0,0 +1,90 @@
+"use client";
+import { getPageVisitsServerAction } from "@/actions/page-visits-actions";
+import useSWR from "swr";
+import useSWRInfinite from "swr/infinite";
+
+interface PageVisitInterface {
+  _id: string;
+  timestamp: string;
+  ipAddress: string;
+  location: {
+    city: string;
+    state_prov: string;
+    country: string;
+    zipcode: string;
+    lat: number;
+    lon: number;
+  } | null;
+  pathname: string;
+}
+
+const fetcher = async (url: string) => {
+  const [_, page, limit] = url.split("/");
+  const { visits, hasMore } = await getPageVisitsServerAction(
+    parseInt(page),
+    parseInt(limit)
+  );
+  return { visits, hasMore };
+};
+
+export default function AdminView() {
+  const limit = 100;
+  const { data, error, size, setSize } = useSWRInfinite(
+    (index) => `/api/page-visits/${index + 1}/${limit}`,
+    fetcher
+  );
+
+  const pageVisits = data ? data.flatMap((data) => data.visits) : [];
+  const isLoading = !data && !error;
+  const hasMore = data && data[data.length - 1].hasMore;
+
+  const handleLoadMore = () => {
+    setSize(size + 1);
+  };
+
+  return (
+    <div className="min-h-screen flex justify-center">
+      <div className="max-w-5xl m-auto mt-8">
+        <h1 className="text-3xl font-medium mb-6">Admin Log View</h1>
+        <table className="w-full max-w-4xl bg-white shadow-md rounded-sm overflow-hidden">
+          <thead>
+            <tr className="bg-gray-100 text-gray-700 text-left">
+              <th className="py-3 pt-4 px-4">Timestamp</th>
+              <th className="py-3 px-4">IP Address</th>
+              <th className="py-3 px-4">Location</th>
+              <th className="py-3 px-4">Pathname</th>
+            </tr>
+          </thead>
+          <tbody>
+            {pageVisits.map((visit) => (
+              <tr key={visit._id} className="text-sm">
+                <td className="py-3 px-4">
+                  {new Date(visit.timestamp).toLocaleString()}
+                </td>
+                <td className="py-3 px-4">{visit.ipAddress}</td>
+                <td className="py-3 px-4">
+                  {visit.location
+                    ? `${visit.location.city}, ${visit.location.country}`
+                    : "-"}
+                </td>
+                <td className="py-3 px-4">{visit.pathname}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+        {isLoading ? (
+          <p className="mt-4">Loading...</p>
+        ) : (
+          hasMore && (
+            <button
+              className="mt-4 px-4 py-2 bg-blue-500 text-white rounded"
+              onClick={handleLoadMore}
+            >
+              Load More
+            </button>
+          )
+        )}
+      </div>
+    </div>
+  );
+}
---
+++ src/lib/check-auth.ts
@@ -0,0 +1,18 @@
+"use client";
+
+export default function checkAuth() {
+  if (typeof window !== "undefined") {
+    const code =
+      !!window && window.localStorage && localStorage.getItem("code");
+    const env_code = process.env.NEXT_PUBLIC_ADMIN_CODE;
+
+    if (!code) {
+      console.log("code is not set");
+      return false;
+    } else if (code === env_code) {
+      console.log("code is correct");
+      return true;
+    }
+  }
+  return false;
+}
