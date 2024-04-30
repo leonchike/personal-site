088eef61a21063479a1af81b5407f6fcdc1418a4
Leon <leonchike@gmail.com>
2024-04-30 12:39:35 -0400

    added posthog

--- src/context/contextProviders.tsx
+++ src/context/contextProviders.tsx
@@ -1,11 +1,19 @@
 "use client";
 
 import { useAnalyticsLogger } from "@/hooks/useAnalyticsLogger";
+import posthog from "posthog-js";
+import { PostHogProvider } from "posthog-js/react";
+
+if (typeof window !== "undefined") {
+  posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY!, {
+    api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST,
+  });
+}
 
 const AppProviders = ({ children }: { children: React.ReactNode }) => {
   useAnalyticsLogger();
 
-  return <>{children}</>;
+  return <PostHogProvider client={posthog}>{children}</PostHogProvider>;
 };
 
 export default AppProviders;
