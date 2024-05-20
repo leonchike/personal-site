7cf161e556935b20d852558bdec51a3d8bbb7041
Leon <leonchike@gmail.com>
2024-05-20 15:26:44 -0400

    updated text

--- src/components/ui/footer.tsx
+++ src/components/ui/footer.tsx
@@ -4,13 +4,10 @@ export default function Footer() {
       <div>
         <p className="text-gray-700 text-[12px]">
           <span className="font-[500]">Disclosure:</span> All work showcased on
-          this personal website has been carefully selected and curated with the
-          explicit permission of the respective companies and organizations
-          involved. To maintain privacy and confidentiality, any sensitive or
-          identifying information within the displayed work has been thoroughly
-          anonymized. Rest assured that the utmost care has been taken to
-          protect the interests of all parties while still providing a
-          comprehensive portfolio that demonstrates my skills and experience.
+          this website has been carefully curated with the explicit permission
+          of the respective companies and organizations involved. To maintain
+          privacy and confidentiality, any sensitive or identifying information
+          has been anonymized or redacted.
         </p>
       </div>
     </footer>
--- src/data/leadership.data.ts
+++ src/data/leadership.data.ts
@@ -14,16 +14,16 @@ export default {
       heading: "60+",
       subheading: "Onboarding sessions",
       description:
-        "Conduct onboarding sessions for new hires, and help them understand the company's mission, vision, and values.",
+        "Conduct onboarding sessions for new hires to help them understand and align with the company's mission, vision, and values.",
     },
   ],
   highlights: [
     "Facilitate workshops on design thinking, product management, and software engineering management",
-    "Instructor (Part-time) at 1863 Ventures, teaching Product Management and Design Thinking to underrepresented founders",
-    "Mentor at the University of Maryland's Startup Shell, helping students in the early stages of their startup journey",
+    "Instructor (part-time) at 1863 Ventures, teaching product management and design thinking to underrepresented founders",
+    "Mentor at the University of Maryland's Startup Shell, a startup and technology incubator, helping students in the early stages of their journey",
     "Led product and design trainings at Deloitte Digital",
-    "Built design systems and led design sprints at Deloitte Digital",
-    "Provided 1:1 mentorship to team members at Deloitte Digital",
+    "Built design systems and led design and engineering sprints at Deloitte Digital",
+    "Provided 1:1 mentorship to colleagues at Deloitte Digital",
     "Entrepreneurial Instructor at the University of Maryland's Dingman Center for Entrepreneurship",
   ],
 };
