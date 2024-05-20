fd0c2ffcd6fb39c1c10a103efe303a765fe2a6f2
Leon <leonchike@gmail.com>
2024-05-20 10:58:40 -0400

    updated email

--- src/data/about.data.ts
+++ src/data/about.data.ts
@@ -6,7 +6,7 @@ export default {
     "Supporting user-facing payments, financial and insurance services and products across a family of microservices. Previously, managed enterprise product development at Deloitte, and led fintech research and development at Capital One. Holds an MBA from Georgetown University and studied Human-Computer Interaction at Stanford.",
   summaryStatement:
     "Product leader by choice, business strategy and finance by education, and a generalist at heart. Over 10 years of experience working in diverse environments, on enterprise and consumer facing digital products across healthcare, media, nonprofits, advertising and Fintech industries. Life-long learner; constantly humbled by the wisdom of many.",
-  contactEmail: "leonchike@gmail.com",
+  contactEmail: "leon.nwankwo@gmail.com",
   contactPhone: "+1 (619) 431-0234",
   contactLocation: "Washington, DC",
   linkedInURL: "https://www.linkedin.com/in/chike-nwankwo/",
