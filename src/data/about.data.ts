004e8c0bd861554e01734e21868e724018f5c698
Leon <leonchike@gmail.com>
2024-05-29 23:16:13 -0400

    updated name data

--- src/data/about.data.ts
+++ src/data/about.data.ts
@@ -1,6 +1,6 @@
 /* eslint-disable import/no-anonymous-default-export */
 export default {
-  name: "Leon Chike Nwankwo",
+  name: "Leon Nwankwo [Chike]",
   currentPosition: "Sr. Technical Product Manager at Rebuildee",
   personalStatement:
     "Supporting user-facing payments, financial and insurance services and products across a family of microservices. Previously, managed enterprise product development at Deloitte, and led fintech research and development at Capital One. Holds an MBA from Georgetown University and studied Human-Computer Interaction at Stanford.",
